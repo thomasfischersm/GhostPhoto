fd0d1b8c5daedbea168320225f4f082bbe45d066
thomasfischersm <thomas@playposse.com>
2017-07-22 15:00:05 -0700

    Add Fabric Crashlytics.

--- app/src/main/java/com/playposse/ghostphoto/activities/ParentActivity.java
+++ app/src/main/java/com/playposse/ghostphoto/activities/ParentActivity.java
@@ -1,17 +1,30 @@
 package com.playposse.ghostphoto.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.playposse.ghostphoto.GhostPhotoApplication;
 import com.playposse.ghostphoto.R;
 
+import io.fabric.sdk.android.Fabric;
+
 /**
  * A common base class for all activities to enable Google Analytics tracking.
  */
 public class ParentActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Fabric.with(this, new Crashlytics());
+    }
 
     @Override
     protected void onResume() {
