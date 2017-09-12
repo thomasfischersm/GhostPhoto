40f5afdbcee28364a08574b3ce99926ea57da299
thomasfischersm <thomas@playposse.com>
2017-09-12 15:25:07 -0700

    Startup time improvement:
    Before: Open intro activity and if not needed, switch to camera.
    After: Open camera activity and if not allowed yet, switch to intro slides.

--- app/src/main/java/com/playposse/ghostphoto/activities/camera/PhotoActivity.java
+++ app/src/main/java/com/playposse/ghostphoto/activities/camera/PhotoActivity.java
@@ -1,15 +1,26 @@
 package com.playposse.ghostphoto.activities.camera;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.playposse.ghostphoto.GhostPhotoPreferences;
 import com.playposse.ghostphoto.activities.ParentActivity;
 import com.playposse.ghostphoto.R;
+import com.playposse.ghostphoto.activities.other.IntroductionActivity;
 
 public class PhotoActivity extends ParentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!GhostPhotoPreferences.hasSeenIntroDeck(this)) {
+            finish();
+            Intent intent = new Intent(getApplicationContext(), IntroductionActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+            return;
+        }
 
         setContentView(R.layout.activity_photo);
 
